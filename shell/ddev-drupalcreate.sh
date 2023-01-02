9e73fee783a8c16933461e74f6f2af23555c257c
Dale McGladdery <dale@group42.ca>
2023-01-02 10:36:35 -0800

    Switch to user regular ports and lock to Drupal 9

--- shell/ddev-drupalcreate.sh
+++ shell/ddev-drupalcreate.sh
@@ -21,13 +21,11 @@ cd $HOMEDIR
 ddev config --project-type=drupal9 \
   --docroot=web \
   --create-docroot \
-  --http-port=8080 \
-  --https-port=8043 \
   --php-version=7.4 \
   --mutagen-enabled
 
 ddev start
-yes | ddev composer create "drupal/recommended-project" --no-install
+yes | ddev composer create "drupal/recommended-project:^9.5.0" --no-install
 ddev composer config allow-plugins true --no-interaction
 ddev composer require drush/drush --no-install
 ddev composer require 'drupal/admin_toolbar:^3.1' --no-install
